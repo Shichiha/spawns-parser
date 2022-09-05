9a2d2f098d9bc4079a00783d942d41d5c7728c1b
Nanako <00sh.chiha+github@gmail.com>
2022-09-06 00:40:31 +0900

    fix missing structs

--- src-rusty/src/main.rs
+++ src-rusty/src/main.rs
@@ -1,6 +1,7 @@
 pub mod structs;
 
 use indicatif::{ProgressBar, ProgressStyle};
+use serde_json::json;
 use serde_json::{Error, Value};
 use std::collections::HashMap;
 use std::fs::File;
@@ -43,41 +44,53 @@ fn parse_scene(d: &Value) -> Scene {
 }
 
 fn main() -> Result<(), Error> {
-    let pb = ProgressBar::new(100);
+    let pb = ProgressBar::new(1000);
     pb.set_style(
         ProgressStyle::default_bar()
-            .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")
-            .unwrap(),
+        .template("[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}")
+        .unwrap(),
     );
-
+    
     let file = File::open("data/Spawns.json")
-        .expect("Couldn't find file 'data/Spawns.json'. is your directory configured properly?");
+    .expect("Couldn't find file 'data/Spawns.json'. is your directory configured properly?");
     let reader = BufReader::new(file);
     let json: Value = serde_json::from_reader(reader)?;
+
+    let mut spawn_count = 0;
+    for scene in json.as_array().unwrap() {
+        spawn_count += scene["spawns"].as_array().unwrap().len();
+    }
+
+    
 
     let mut scenes: Vec<Scene> = Vec::new();
     pb.set_message("Parsing scenes");
     for scene in json.as_array().unwrap() {
         scenes.push(parse_scene(scene));
     }
+    let mut parsed_spawn_count = 0;
 
-    let mut parsed_spawns: HashMap<String, Vec<Vec3>> = HashMap::new();
+    let mut parsed_spawns: HashMap<i64, Vec<Vec3>> = HashMap::new();
     pb.set_message("Parsing spawns");
     for scene in scenes {
         for spawn in scene.spawns {
-            if !parsed_spawns.contains_key(&spawn.monster_id.to_string()) {
-                parsed_spawns.insert(spawn.monster_id.to_string(), Vec::new());
-            } else {
-                parsed_spawns
-                    .get_mut(&spawn.monster_id.to_string())
-                    .unwrap()
-                    .push(spawn.pos)
+            if !parsed_spawns.contains_key(&spawn.monster_id) {
+                parsed_spawns.insert(spawn.monster_id, Vec::new());
             }
+            parsed_spawns.get_mut(&spawn.monster_id).unwrap().push(spawn.pos);
+            parsed_spawn_count += 1;
         }
     }
 
+    if spawn_count != parsed_spawn_count {
+        panic!(
+            "Spawn count mismatch! Original: {}, Parsed: {}",
+            spawn_count, parsed_spawn_count
+        );
+    }
+
     pb.set_message("Writing to file");
-    let mut file = File::create("parsed/parsedSpawns.json").unwrap();
+    let mut file = File::create("parsed/parsedSpawns.json").expect("Couldn't create file");
     file.write(serde_json::to_string(&parsed_spawns).unwrap().as_bytes())
         .unwrap();
 

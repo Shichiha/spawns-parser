ff7ff7412272df71cde4ca34a2c6c9abccd702a5
Nanako <00sh.chiha+github@gmail.com>
2022-09-24 14:21:29 +0900

    simplify c++

--- src-cpp/main.cpp
+++ src-cpp/main.cpp
@@ -13,18 +13,15 @@ int main()
 
     nlohmann::json parsed_spawns;
     int parsed_spawn_count = 0;
-    for (auto& scene : spawn_data)
-    {
-        for (auto& spawn : scene["spawns"])
+    for (auto &scene : spawn_data)
+        for (auto &spawn : scene["spawns"])
         {
-            if (parsed_spawns.find(std::to_string(spawn["monsterId"].get<unsigned int>())) == parsed_spawns.end())
-            {
-                parsed_spawns[std::to_string(spawn["monsterId"].get<unsigned int>())] = nlohmann::json::array();
-            }
-            parsed_spawns[std::to_string(spawn["monsterId"].get<unsigned int>())].push_back({ spawn["pos"]["x"], spawn["pos"]["y"], spawn["pos"]["z"] });
+            auto monster_id = spawn["monsterId"].get<unsigned int>();
+            if (parsed_spawns.find(std::to_string(monster_id)) == parsed_spawns.end())
+                parsed_spawns[std::to_string(monster_id)] = nlohmann::json::array();
+            parsed_spawns[std::to_string(monster_id)].push_back({spawn["pos"]["x"], spawn["pos"]["y"], spawn["pos"]["z"]});
             parsed_spawn_count++;
         }
-    }
 
     std::ofstream parsed_spawn_file("../parsed/parsedSpawns.json");
     parsed_spawn_file << parsed_spawns.dump(4);
